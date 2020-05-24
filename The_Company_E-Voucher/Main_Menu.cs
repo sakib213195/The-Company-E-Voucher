c15f135f096bd04aafcbe84f2bf8dbe9363c9844
Muhammad Sakib Khan <sakib2131@gmail.com>
2020-05-24 22:15:48 +0600

    Expense Voucher added and DB Updated

--- The_Company_E-Voucher/Main_Menu.cs
+++ The_Company_E-Voucher/Main_Menu.cs
@@ -38,5 +38,13 @@ namespace The_Company_E_Voucher
             Visible = false;
 
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Expense_Voucher openForm = new Expense_Voucher();
+            openForm.Show();
+            Visible = false;
+
+        }
     }
 }
